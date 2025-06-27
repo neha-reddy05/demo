1aff9045eedaefc57c7c83e47df971114ca4e97a
neha-reddy05 <nehalokireddy5@gmail.com>
2025-06-27 09:02:20 +0530

    branc s added

---
+++ react/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <h2 className="text-2xl text-blue-500">Hello World</h2>
+    </>
+  )
+}
+
+export default App
